a9d52a8b5e2d571fc5aaaff4967eab2dc6eb4c93
ichsanputr <ngekode24@gmail.com>
2026-01-18 14:39:35 +0700

    a

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,16 +2,16 @@
 module.exports = {
   apps: [
     {
-      name: 'budibadu',
+      name: 'archeryhub',
       script: './.output/server/index.mjs',
       cwd: './',
       exec_mode: 'fork', // 👈 non-cluster mode (single process)
       env: {
         NODE_ENV: 'production',
-        PORT: 9000,
-        NITRO_PORT: 9000,
+        PORT: 9001,
+        NITRO_PORT: 9001,
         NITRO_HOST: '0.0.0.0',
-        NUXT_PUBLIC_API_BASE_URL: 'https://api.budibadu.com'
+        NUXT_PUBLIC_API_BASE_URL: 'https://api.archeryhub.id'
       }
     },
   ],
